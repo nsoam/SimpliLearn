89e1e481640b01a36c2268a3bd687da186d76e3e
nsoam <soam.neha@gmail.com>
2021-11-06 21:02:58 -0500

    Section 2.3 - Method Example

---
+++ Calculator/src/MethodEx.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MethodEx {
+    public int add(int a, int b) {
+        int c = a+b;
+        System.out.println("a+b= "+ c);
+        return c;
+    }
+
+    public int sub(int a, int b) {
+        int c = a-b;
+        System.out.println("a-b= "+ c);
+        return c;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        MethodEx methodEx = new MethodEx();
+        System.out.println("a= "+ a);
+        System.out.println("b= "+ b);
+        methodEx.add(a,b);
+        methodEx.sub(a,b);
+    }
+}
